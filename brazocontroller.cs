f83c3caa1e2efd4bec3e81f3293367fa06850c0e
BRD-Desing <65409913+BRD-Desing@users.noreply.github.com>
2020-07-04 19:19:32 -0600

    Actualización de "CY"
    Se añade la última habilidad de movimiento, se arregla el disparo del personaje evitando que durante la habilidad de movimiento se pueda disparar.

--- brazocontroller.cs
+++ brazocontroller.cs
@@ -14,7 +14,7 @@ public class brazocontroller : MonoBehaviour {
     public bool invis = false;
     private bool meleeMode;
 
-    private bool tocandoSuperficie;
+    public bool tocandoSuperficie;
     public float radio;
 
     public bool meleeMo = false;
@@ -42,13 +42,21 @@ public class brazocontroller : MonoBehaviour {
     public float delayT;
     private float coldownFuego;
 
-
+    public bool habilidadDeMovimiento;
 
     //Balas por segundo
     private int BFS;
-    public bool clic = false;
+    private bool clic;
+    public bool clk;
     //meta = metamorfosis
     private bool meta;
+
+    //Disparo del rifle
+    public float tiempoEntreBalasDeEsnipers;
+    private float referenciaSniper;
+
+    public GameObject sniperBullet;
+
     private bool disparando;
 
     private void Start()
@@ -61,15 +69,36 @@ public class brazocontroller : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        clic = clk;
 
         //Personaje
         anim.SetBool("invis", invis);
         anim.SetBool("MeleeMod", meleeMo);
-        anim.SetBool("meta", meta);
+        anim.SetInteger("tipoDisparo", tipoDeBala);
         anim.SetBool("disparando", clic);
         anim.SetBool("plasma", plasma);
 
         tipoHabilidadDisparo = pesonaje.GetComponent<playercontroller>().TipoHabilidad;
+
+        habilidadDeMovimiento = pesonaje.GetComponent<playercontroller>().MovimientoEnProceso;
+
+        enLaHabilidad = pesonaje.GetComponent<playercontroller>().EnLaHabilidad;
+
+        if (habilidadDeMovimiento == false)
+        {
+            invis = enLaHabilidad;
+        }
+        if (habilidadDeMovimiento == true)
+        {
+            invis = habilidadDeMovimiento;
+        }
+
+        meleeMo = pesonaje.GetComponent<playercontroller>().meleeMode;
+
+
+
+
+
 
         if (meleeMode == true)
         {
@@ -78,7 +107,7 @@ public class brazocontroller : MonoBehaviour {
 
         tocandoSuperficie = Physics2D.OverlapCircle(generador.transform.position, radio, queEsTierra);
 
-        if (meleeMo == false)
+        if (meleeMo == false && invis == false)
         {
             //Mira moviendose
             MousePosition = Input.mousePosition;
@@ -87,141 +116,157 @@ public class brazocontroller : MonoBehaviour {
             angulo = Mathf.Atan2((MousePosition.y - ObjetoPosition.y), (MousePosition.x - ObjetoPosition.x)) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angulo));
 
-            
-
-            if (tipoDeBala == 1)
+            if (tipoDeBala == 1 && enLaHabilidad == false)
             {
-                 Disparo1();
+                Disparo1();
+            }
+
+            //reduccion de tiempos
+            if (tiempoEspera > 0)
+            {
+                tiempoEspera -= Time.deltaTime;
             }
 
             if (tocandoSuperficie == false)
             {
-
                 //entrada de disparo
-                if (Input.GetButtonDown("Fire1"))
-                {
-                    disparando = true;
-                }
-                else if (Input.GetButtonUp("Fire1"))
-                {
-                    disparando = false;
-                }
-
-                //reduccion de tiempos
-                if (tiempoEspera > 0)
-                {
-                    tiempoEspera -= Time.deltaTime;
-                }
+                disparando = Input.GetButton("Fire1");
 
 
-                if (delayT > 0)
-                {
-                    delayT -= Time.deltaTime;
-                    tiempoEspera = 0.0001f;
-                }
-                else if (delayT <= 0)
-                {
-                    BFS = 0;
-                }
-                if (coldownFuego > 0)
-                {
-                    coldownFuego -= Time.deltaTime;
-                }
-
-                if (tocandoSuperficie == true || delayT <= 0)
-                {
-                    CancelInvoke("EsferaMetamorfosis");
-
-                }
-                if (BFS == 1 && delayT <= 0)
-                {
-                    coldownFuego = 0.5f;
-                    clic = false;
-                }
+                //   MMMMM
+                //  MM   MM
+                //      MM
+                //    MMM
+                //  MMMMMMMM
 
 
-                if (tipoDeBala == 2 && enLaHabilidad == false)
+                    if (delayT > 0)
+                    {
+                        delayT -= Time.deltaTime;
+                        tiempoEspera = 0.0001f;
+                    }
+                    else if (delayT <= 0)
+                    {
+                        BFS = 0;
+                    }
+                    if (coldownFuego > 0)
+                    {
+                        coldownFuego -= Time.deltaTime;
+                    }
+
+                    if (tocandoSuperficie == true || delayT <= 0)
+                    {
+                        CancelInvoke("EsferaMetamorfosis");
+
+                    }
+                    if (BFS == 1 && delayT <= 0)
+                    {
+                        coldownFuego = 0.5f;
+                        clk = false;
+                    }
+
+                if (tipoDeBala == 2 && invis == false)
                 {
                     meta = true;
 
-                    if (disparando)
+
+                    if (disparando && tocandoSuperficie == false)
                     {
                         if (invis == false && delayT <= 0 && coldownFuego <= 0 && clic == false)
                         {
                             BFS = 1;
                             delayT = 0.3f;
-                            clic = true;
+                            clk = true;
+                        }
+                        if (BFS == 1 && delayT > 0 && tiempoEspera <= 0)
+                        {
+                            InvokeRepeating("EsferaMetamorfosis", 0.0001f, 0.13f);
                         }
                     }
 
-                   
-                    if (BFS == 1 && delayT > 0 && tiempoEspera <= 0)
+
+                    //  MMMMMM
+                    // MM    MM
+                    //     MMM
+                    // MM    MM
+                    //  MMMMMM
+
+                    if (tipoDeBala == 3 && enLaHabilidad == false)
                     {
-                        InvokeRepeating("EsferaMetamorfosis", 0.0001f, 0.13f);
+                        if (referenciaSniper != tiempoEntreBalasDeEsnipers)
+                        {
+                            referenciaSniper += Time.deltaTime;
+                        }
+
+
+
+                        if (disparando == true && referenciaSniper == tiempoEntreBalasDeEsnipers)
+                        {
+                            referenciaSniper = 0;
+
+                            Instantiate(sniperBullet, generador.position, generador.rotation);
+                        }
                     }
                 }
+                 
 
-             
-
+                Plasma();
 
             }
 
-            Plasma();
-            
-        }
 
 
-
-        if (mira.position.x < gameObject.transform.position.x)
-        {
-            render.flipY = true;
-        }
-        else if (mira.position.x > gameObject.transform.position.x)
-        {
-            render.flipY = false;
-        }
-    }
-
-
-    //Variables de disparos normales [importante]
-
-    //variable del disparo 1
-
-    private void Disparo1()
-    {
-        if (enLaHabilidad == false)
-        {
-            meta = false;
-            if (Input.GetButtonDown("Fire1") && invis == false)
+            if (mira.position.x < gameObject.transform.position.x)
             {
-                InvokeRepeating("Fire", 0.0001f, 0.15f);
+                render.flipY = true;
             }
-            else if (Input.GetButtonUp("Fire1") || invis == true)
+            else if (mira.position.x > gameObject.transform.position.x)
             {
-                CancelInvoke("Fire");
+                render.flipY = false;
             }
         }
     }
-    void Fire()
-    {
-        Instantiate(disparo, generador.position, generador.rotation);
-    }
+
+
+        //Variables de disparos normales [importante]
+
+        //variable del disparo 1
+
+        void Disparo1()
+        {
+            if (enLaHabilidad == false && invis == false)
+            {
+                meta = false;
+                if (Input.GetButtonDown("Fire1") && invis == false)
+                {
+                    InvokeRepeating("Fire", 0.0001f, 0.15f);
+                }
+                else if (Input.GetButtonUp("Fire1") || invis == true)
+                {
+                    CancelInvoke("Fire");
+                }
+            }
+        }
+        void Fire()
+        {
+            Instantiate(disparo, generador.position, generador.rotation);
+        }
 
 
 
 
-    //Disparo 2
+        //Disparo 2
 
-    private void EsferaMetamorfosis()
-    {
-        Instantiate(esferaMeta, generador.position, generador.rotation);
-    }
+        private void EsferaMetamorfosis()
+        {
+            Instantiate(esferaMeta, generador.position, generador.rotation);
+        }
 
-    //Disparo del plasma
-    private void DisparoPlasma()
-    {
-        Instantiate(plasmaGame, generador.position, generador.rotation);
-    }
+        //Disparo del plasma
+        private void DisparoPlasma()
+        {
+            Instantiate(plasmaGame, generador.position, generador.rotation);
+        }
 
 
 
@@ -230,6 +275,7 @@ public class brazocontroller : MonoBehaviour {
     private bool enLaHabilidad;
     private float tiempoEntreBalas;
     private bool plasma;
+
     private void Plasma()
     {
         if (tiempoEntreBalas > 0)
